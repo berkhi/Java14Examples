fc5800c18799efb97f500672f0c0acf693fcb6cd
berkin.yardimci <berkinyardimci98@gmail.com>
2024-01-19 16:59:07 +0300

    Refactor Code

--- src/main/java/com/bilgeadam/lesson004/Question21.java
+++ src/main/java/com/bilgeadam/lesson004/Question21.java
@@ -13,19 +13,53 @@ public class Question21 {
         System.out.println("Sayı girin");
         int sayi = scanner.nextInt();
 
-        for (int i = 0; i < sayi; i+=2) {
-            if(sayi %2 == 0){
-                if(i == sayi - 2){
-                    System.out.print(i);;
-                }else {
+        for (int i = 0; i < sayi; i += 2) {
+            if (sayi % 2 == 0) {
+                if (i == sayi - 2) {
+                    System.out.print(i);
+                    ;
+                } else {
                     System.out.print(i + " ,");
                 }
-            }else {
-                if(i == sayi - 1){
-                    System.out.print(i);;
-                }else {
+            } else {
+                if (i == sayi - 1) {
+                    System.out.print(i);
+                } else {
                     System.out.print(i + " ,");
                 }
+            }
+        }
+
+
+        System.out.println("----------------------");
+
+        System.out.println("Bir Sayı Giriniz");
+        int girilenSayi = scanner.nextInt();
+
+        for (int i = 0; i <= girilenSayi; i += 2) {
+            System.out.print(i);
+            //100
+            //i = 100
+            // 101 < 100
+            if ((i + 1 < girilenSayi)) {
+                System.out.print(", ");
+            }
+        }
+
+
+        System.out.println("----------------");
+        System.out.println("Sayı Giriniz");
+        int sayi1 = scanner.nextInt();
+
+
+        if (sayi1 % 2 != 0) {
+            sayi1--;
+        }
+        for (int i = 0; i <= sayi1; i += 2) {
+            if (i == sayi1) {
+                System.out.print(i);
+            } else {
+                System.out.print(i + ", ");
             }
         }
     }
